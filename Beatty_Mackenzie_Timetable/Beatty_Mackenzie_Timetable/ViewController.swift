f0aadd3af37c1c58d5e218971a87c33820b3c233
perceptionfoundry <perception.khi@gmail.com>
2018-06-05 17:18:57 +0500

    new commit

--- Beatty_Mackenzie_Timetable/Beatty_Mackenzie_Timetable/ViewController.swift
+++ Beatty_Mackenzie_Timetable/Beatty_Mackenzie_Timetable/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+print("git remote add origin git@github.com:perceptionfoundry/Beatty_Mackenzie_Timetable.git")
     }
 
 
