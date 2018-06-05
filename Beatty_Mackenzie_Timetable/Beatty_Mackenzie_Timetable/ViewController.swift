45eaa2773cfef11dea6ec667816336b96b0e67df
perceptionfoundry <perception.khi@gmail.com>
2018-06-05 17:17:04 +0500

    first

---
+++ Beatty_Mackenzie_Timetable/Beatty_Mackenzie_Timetable/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Beatty_Mackenzie_Timetable
+//
+//  Created by Syed ShahRukh Haider on 05/06/2018.
+//  Copyright © 2018 Syed ShahRukh Haider. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
